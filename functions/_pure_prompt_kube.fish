d683c29f0475563deb0dcd88488bf1ea7dea902c
Jay Morrow <takuya813@gmail.com>
2021-04-03 14:30:50 -0700

    fix kube cmd from merge changes

--- functions/_pure_prompt_kube.fish
+++ functions/_pure_prompt_kube.fish
@@ -1,7 +1,7 @@
 function _pure_prompt_kube
     set --query --global kubeenv
     or set --local kubeenv (kubectl config current-context 2>/dev/null | grep -v dev)
-    set --local kubeenv_color "$pure_color_kube"
+    set --local kubeenv_color (_pure_set_color $pure_color_kube)
 
     echo "$kubeenv_color$kubeenv"
 
